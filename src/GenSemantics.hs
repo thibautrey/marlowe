1d9fb6ea2cb4d9b176c1537df033834096a3b6b5
Pablo Lamela Seijas <palas87@gmail.com>
2018-10-29 14:45:33 +0000

    Modify generators to make using positive numbers only easy

--- src/GenSemantics.hs
+++ src/GenSemantics.hs
@@ -1,20 +1,23 @@
 module GenSemantics where
 
-import Data.Set (Set)
-import qualified Data.Set as S
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as M
-
 import Semantics
 import Test.QuickCheck
+import qualified Data.Map as M
+import qualified Data.Set as S
+
+positiveAmount :: Gen Integer
+positiveAmount = fmap abs arbitrary
+
+-- Comment out this line to test with negative values too
+amount = positiveAmount
 
 arbitraryMoneyAux :: Int -> Gen Money
 arbitraryMoneyAux s
- | s > 0 = oneof [(AvailableMoney . IdentCC) <$> arbitrary
+ | s > 0 = oneof [(AvailableMoney . IdentCC) <$> amount
                  ,(AddMoney <$> arbitraryMoneyAux (s - 1)) <*> arbitraryMoneyAux (s - 1)
-                 ,ConstMoney <$> arbitrary
-                 ,(MoneyFromChoice . IdentChoice) <$> arbitrary <*> arbitrary <*> arbitraryMoneyAux (s - 1)]
- | s == 0 = oneof [(AvailableMoney . IdentCC) <$> arbitrary
+                 ,ConstMoney <$> amount
+                 ,(MoneyFromChoice . IdentChoice) <$> amount <*> amount <*> arbitraryMoneyAux (s - 1)]
+ | s == 0 = oneof [(AvailableMoney . IdentCC) <$> amount
                   ,ConstMoney <$> arbitrary]
  | otherwise = error "Negative size in arbitraryMoney"
  
@@ -23,17 +26,17 @@ arbitraryMoney = sized arbitraryMoneyAux
 
 arbitraryObservationAux :: Int -> Gen Observation
 arbitraryObservationAux s
- | s > 0 = oneof [BelowTimeout <$> arbitrary
+ | s > 0 = oneof [BelowTimeout <$> amount
                  ,AndObs <$> arbitraryObservationAux (s - 1) <*> arbitraryObservationAux (s - 1) 
                  ,OrObs <$> arbitraryObservationAux (s - 1) <*> arbitraryObservationAux (s - 1)
                  ,NotObs <$> arbitraryObservationAux (s - 1)  
-                 ,(PersonChoseThis . IdentChoice) <$> arbitrary <*> arbitrary <*>  arbitrary
-                 ,(PersonChoseSomething . IdentChoice) <$> arbitrary <*> arbitrary
+                 ,(PersonChoseThis . IdentChoice) <$> amount <*> amount <*> amount
+                 ,(PersonChoseSomething . IdentChoice) <$> amount <*> amount
                  ,ValueGE <$> arbitraryMoneyAux (s - 1) <*> arbitraryMoneyAux (s - 1)
                  ,pure TrueObs,pure FalseObs]
- | s == 0 = oneof [BelowTimeout <$> arbitrary
-                  ,(PersonChoseThis . IdentChoice) <$> arbitrary <*> arbitrary <*> arbitrary
-                  ,(PersonChoseSomething . IdentChoice) <$> arbitrary <*> arbitrary
+ | s == 0 = oneof [BelowTimeout <$> amount
+                  ,(PersonChoseThis . IdentChoice) <$> amount <*> amount <*> amount
+                  ,(PersonChoseSomething . IdentChoice) <$> amount <*> amount
                   ,pure TrueObs,pure FalseObs]
  | otherwise = error "Negative size in arbitraryObservation"
 
@@ -44,12 +47,12 @@ arbitraryObservation = sized arbitraryObservationAux
 arbitraryContractAux :: Int -> Gen Contract
 arbitraryContractAux s
  | s > 0 = oneof [pure Null
-                 ,(CommitCash . IdentCC) <$> arbitrary <*> arbitrary <*> arbitraryMoneyAux (s - 1) <*> arbitrary <*> arbitrary <*> arbitraryContractAux (s - 1) <*> arbitraryContractAux (s - 1) 
-                 ,(RedeemCC . IdentCC) <$> arbitrary <*> arbitraryContractAux (s - 1)
-                 ,(Pay . IdentPay) <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitraryMoneyAux (s - 1) <*> arbitrary <*> arbitraryContractAux (s - 1)
+                 ,(CommitCash . IdentCC) <$> amount <*> amount <*> arbitraryMoneyAux (s - 1) <*> amount <*> amount <*> arbitraryContractAux (s - 1) <*> arbitraryContractAux (s - 1) 
+                 ,(RedeemCC . IdentCC) <$> amount <*> arbitraryContractAux (s - 1)
+                 ,(Pay . IdentPay) <$> amount <*> amount <*> amount <*> arbitraryMoneyAux (s - 1) <*> amount <*> arbitraryContractAux (s - 1)
                  ,Both <$> arbitraryContractAux (s - 1) <*> arbitraryContractAux (s - 1)
                  ,Choice <$> arbitraryObservationAux (s - 1) <*> arbitraryContractAux (s - 1) <*> arbitraryContractAux (s - 1)
-                 ,When <$> arbitraryObservationAux (s - 1) <*> arbitrary <*> arbitraryContractAux (s - 1) <*> arbitraryContractAux (s - 1)]
+                 ,When <$> arbitraryObservationAux (s - 1) <*> amount <*> arbitraryContractAux (s - 1) <*> arbitraryContractAux (s - 1)]
  | s == 0 = oneof [pure Null]
  | otherwise = error "Negative size in arbitraryObservation"
 
@@ -57,16 +60,16 @@ arbitraryContract :: Gen Contract
 arbitraryContract = sized arbitraryContractAux
 
 arbitraryCC :: Gen CC
-arbitraryCC = (CC . IdentCC) <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+arbitraryCC = (CC . IdentCC) <$> amount <*> amount <*> amount <*> amount
 
 arbitraryRC :: Gen RC
-arbitraryRC = (RC . IdentCC) <$> arbitrary <*> arbitrary <*> arbitrary
+arbitraryRC = (RC . IdentCC) <$> amount <*> amount <*> amount
 
 arbitraryRPEntry :: Gen ((IdentPay, Person), Cash)
-arbitraryRPEntry = (\x y z -> ((IdentPay x, y), z)) <$> arbitrary <*> arbitrary <*> arbitrary
+arbitraryRPEntry = (\x y z -> ((IdentPay x, y), z)) <$> amount <*> amount <*> amount
 
 arbitraryICEntry :: Gen ((IdentChoice, Person), ConcreteChoice)
-arbitraryICEntry = (\x y z -> ((IdentChoice x, y), z)) <$> arbitrary <*> arbitrary <*> arbitrary
+arbitraryICEntry = (\x y z -> ((IdentChoice x, y), z)) <$> amount <*> amount <*> amount
 
 arbitraryInputAux :: Int -> Gen Input
 arbitraryInputAux s = (\w x y z -> Input (S.fromList w) (S.fromList x) (M.fromList y) (M.fromList z))
@@ -74,5 +77,6 @@ arbitraryInputAux s = (\w x y z -> Input (S.fromList w) (S.fromList x) (M.fromLi
 
 arbitraryInput :: Gen Input
 arbitraryInput = sized arbitraryInputAux
+
 
 
